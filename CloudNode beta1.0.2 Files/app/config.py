080bebac24bb7b3d606f7050727a92aa6388f2b1
dawidlouisbekker <EDUV4947097@vossie.net>
2024-09-15 17:49:54 +0200

    Update config.py

--- CloudNode beta1.0.2 Files/app/config.py
+++ CloudNode beta1.0.2 Files/app/config.py
@@ -1,9 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = "sqlite:///./database.sqlite3"        #os.getenv("DATABASE_URL", "sqlite:///./database.db")
-SECRET_KEY =  'iox-dIJ-928P2F0[W9JAS]'             #os.getenv("SECRET_KEY", "your-secret-key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+####
